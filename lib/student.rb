4731099c3753300c05a81d8d9d25469f7d741a99
Amber Taranto <ambzzzz@gmail.com>
2019-08-08 00:49:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,12 +3,15 @@ require_relative "./user.rb"
 
 class Student < User
 
+    attr_reader :knowledge
+
     def initialize
         @knowledge = []
     end
 
     def learn(knowledge)
         @knowledge << knowledge
-        
+
     end
+
 end
